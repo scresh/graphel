6e4759e309628794166e07e06e745750eb8e4195
scresh <screshowski@gmail.com>
2020-01-30 20:30:20 +0100

    Move country to Airport model

--- tools/database.py
+++ tools/database.py
@@ -12,7 +12,7 @@ _DATABASE = SqliteDatabase(None)
 
 def create_database(db_filename):
     _DATABASE.init(db_filename)
-    _DATABASE.create_tables([Country, Airport, Flight, Distance, Chain, ChainCountry, ChainFlight])
+    _DATABASE.create_tables([Airport, Flight, Distance, Chain, ChainFlight])
 
     return _DATABASE
 
@@ -22,13 +22,9 @@ class BaseModel(Model):
         database = _DATABASE
 
 
-class Country(BaseModel):
-    code = FixedCharField(unique=True, max_length=2)
-
-
 class Airport(BaseModel):
     code = FixedCharField(unique=True, max_length=3)
-    country = ForeignKeyField(Country)
+    country = FixedCharField(unique=True, max_length=2)
     latitude = DecimalField(max_digits=3, decimal_places=2)
     longitude = DecimalField(max_digits=3, decimal_places=2)
 
@@ -65,18 +61,6 @@ class Chain(BaseModel):
     cost = DecimalField(max_digits=6, decimal_places=2)
 
 
-class ChainCountry(BaseModel):
-    chain = ForeignKeyField(Chain)
-    country = ForeignKeyField(Country)
-
-    class Meta:
-        indexes = (
-            (('chain', 'country'), True),
-            (('chain',), False),
-            (('country',), False),
-        )
-
-
 class ChainFlight(BaseModel):
     chain = ForeignKeyField(Chain)
     flight = ForeignKeyField(Flight)
@@ -87,3 +71,34 @@ class ChainFlight(BaseModel):
             (('chain',), False),
             (('flight',), False),
         )
+
+
+def get_matching_flights(recent_airport_code, amount_left, lower_range, upper_range):
+    return (
+        Flight.select()
+        .join(Airport, on=Flight.source)
+        .where(Flight.source.code == recent_airport_code)
+        .where(Flight.cost <= amount_left)
+        .where(Flight.date.between(lower_range, upper_range))
+    )
+
+
+def get_recent_airport_and_date(chain_id):
+    recent_flight = (
+        ChainFlight.select().where(ChainFlight.chain_id == chain_id)
+        .order_by(ChainFlight.id.desc()).limit(1)
+    )[0].flight
+
+    return recent_flight.destination.code, recent_flight.date
+
+
+def get_new_chain(old_chain, new_flight):
+    new_cost = old_chain.cost + new_flight.cost
+    new_chain = Chain.get(Chain.id == Chain.insert(cost=new_cost).execute())
+
+    for chain_flight in ChainFlight.select().where(ChainFlight.chain_id == old_chain.id).order_by(ChainFlight.id):
+        ChainFlight.insert(chain_id=new_chain.id, flight_id=chain_flight.flight_id).execute()
+
+    ChainFlight.insert(chain_id=new_chain.id, flight_id=new_flight.id).execute()
+
+    return new_chain
